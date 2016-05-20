35795bc77cb6725853ad0ddc2d3ff67e373dd07a
AiWelv <AiWelv@163.com>
2016-05-20 17:36:05 +0800

    权限调整

--- service/src/main/java/com/huotu/agento2o/service/common/AuthorityEnum.java
+++ service/src/main/java/com/huotu/agento2o/service/common/AuthorityEnum.java
@@ -13,9 +13,7 @@ public enum AuthorityEnum implements ICommonEnum {
     ROLE_ORDER("ROLE_ORDER","订单"),
     ROLE_PURCHASE("ROLE_PURCHASE","我的采购"),
     ROLE_AGENT_PURCHASE("ROLE_AGENT_PURCHASE","下级采购"),
-    ROLE_BASE_DATA("ROLE_BASE_DATA","基本配置");
-    ROLE_AGENT_PURCHASE("ROLE_AGENT_PURCHASE","下级采购"),
-    ROLE_BASE_DATA("ROLE_BASE_DATA","基本设置"),
+    ROLE_BASE_DATA("ROLE_BASE_DATA","基本配置"),
     ROLE_BASE_SHOP("ROLE_BASE_SHOP","门店基本设置");
     private String code;
     private String value;
