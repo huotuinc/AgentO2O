d7884ac97892e71f51cd6d3d40a385c965a36e6f
wuxiongliu <xiong328160186@qq.com>
2016-05-25 17:20:53 +0800

    modify  单元测试

--- service/src/test/java/com/huotu/agento2o/service/service/purchase/impl/AgentReturnedOrderServiceTest.java
+++ service/src/test/java/com/huotu/agento2o/service/service/purchase/impl/AgentReturnedOrderServiceTest.java
@@ -1,9 +1,8 @@
 package com.huotu.agento2o.service.service.purchase.impl;
 
 import com.huotu.agento2o.common.util.SerialNo;
+import com.huotu.agento2o.service.entity.MallCustomer;
 import com.huotu.agento2o.service.entity.author.Agent;
-import com.huotu.agento2o.service.entity.author.Author;
-import com.huotu.agento2o.service.entity.purchase.AgentProduct;
 import com.huotu.agento2o.service.entity.purchase.AgentReturnedOrder;
 import com.huotu.agento2o.service.repository.purchase.AgentReturnOrderItemRepository;
 import com.huotu.agento2o.service.repository.purchase.AgentReturnOrderRepository;
@@ -13,9 +12,8 @@ import com.huotu.agento2o.service.service.purchase.AgentReturnedOrderService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -32,64 +30,70 @@ public class AgentReturnedOrderServiceTest extends CommonTestBase {
     @Autowired
     private AgentReturnOrderItemRepository agentReturnOrderItemRepository;
 
-
     @Test
-    public void testFindAgentProductsByAgentId(){
-        Integer agentId = -1;
-        List<AgentProduct> agentProducts =  agentReturnedOrderService.findAgentProductsByAgentId(agentId);
-        Assert.assertTrue(agentProducts.size()==0);
+    public void testFindOne(){
 
-        agentId = null;
-        agentProducts =  agentReturnedOrderService.findAgentProductsByAgentId(agentId);
-        Assert.assertTrue(agentProducts.size()==0);
+        AgentReturnedOrder agentReturnedOrder = new AgentReturnedOrder();
+        String rOrderId = SerialNo.create();
+        agentReturnedOrder.setROrderId(rOrderId);
 
-        agentId = 1;
-        agentProducts =  agentReturnedOrderService.findAgentProductsByAgentId(agentId);
-        Assert.assertTrue(agentProducts.size()>=0);
-    }
+        agentReturnOrderRepository.save(agentReturnedOrder);
 
-    @Test
-    public void testFindAll(){
-        ReturnedOrderSearch returnedOrderSearch = new ReturnedOrderSearch();
+        AgentReturnedOrder agentReturnedOrder1 = agentReturnedOrderService.findOne(rOrderId);
+        Assert.assertNotNull(agentReturnedOrder1);
 
-//        returnedOrderSearch.setAgentId(2);
-        returnedOrderSearch.setParentAgentId(71);
+        agentReturnedOrder1 = agentReturnedOrderService.findOne("");
+        Assert.assertNull(agentReturnedOrder1);
 
-
-        Author author = new Agent();
-        author.setId(2);
-
-        Page<AgentReturnedOrder> agentReturnedOrderPage = agentReturnedOrderService.findAll(1,10,author,returnedOrderSearch);
-
-        List<AgentReturnedOrder> agentReturnedOrders = agentReturnedOrderPage.getContent();
-
-        System.out.println("**************");
-        agentReturnedOrderPage.forEach(agentReturnedOrder -> {
-            System.out.println(agentReturnedOrder.getROrderId());
-        });
-        System.out.println("**************");
-
+        agentReturnedOrder1 = agentReturnedOrderService.findOne(null);
+        Assert.assertNull(agentReturnedOrder1);
     }
 
     @Test
     public void testAddReturnOrder(){
         AgentReturnedOrder agentReturnedOrder = new AgentReturnedOrder();
-        agentReturnedOrder.setROrderId(SerialNo.create());
-        agentReturnedOrder.setCreateTime(new Date());
+        String rOrderId = SerialNo.create();
+        agentReturnedOrder.setROrderId(rOrderId);
+
         agentReturnedOrderService.addReturnOrder(agentReturnedOrder);
+
+        AgentReturnedOrder agentReturnedOrder1 = agentReturnOrderRepository.findOne(rOrderId);
+        Assert.assertNotNull(agentReturnedOrder1);
+
+        agentReturnedOrder1 = agentReturnedOrderService.addReturnOrder(null);
+        Assert.assertNull(agentReturnedOrder1);
+
+    }
+
+    /**
+     *  构造退货单列表用于分页查询接口测试
+     *  构造一个一级代理商
+     *  构造一个二级代理商
+     *  平台查询所有一级代理商退货列表
+     *  一级代理商查询下级退货单列表
+     *  代理商查看自己的退货单列表
+     *
+     */
+    private void createAgentReturnOrderList(){
+        MallCustomer mallCustomer = mockMallCustomer();
+        Agent parentAgent1 = mockAgent(mallCustomer,null);
+        Agent parentAgent2 = mockAgent(mallCustomer,null);
+        Agent agent1 = mockAgent(mallCustomer,parentAgent1);
+        Agent agent2 = mockAgent(mallCustomer,parentAgent1);
+
+        List<AgentReturnedOrder> parentAgent1ROrders = new ArrayList<>();
+
+
+
     }
 
     @Test
-    public void testCancelReturnOrder(){
-        String rOrderId = "20160520094427115743";
-        agentReturnedOrderService.cancelReturnOrder(rOrderId);
+    public void testFindAll(){
+        ReturnedOrderSearch returnedOrderSearch = new ReturnedOrderSearch();
+        agentReturnedOrderService.findAll(returnedOrderSearch);
     }
 
-    @Test
-    public void testFindByRorderId(){
-        String rOrderId = "20160520094427115743";
-        AgentReturnedOrder agentReturnedOrder = agentReturnOrderRepository.findByROrderIdAndDisabledFalse(rOrderId);
-        System.out.println(agentReturnedOrder);
-    }
+
+
 
 }
